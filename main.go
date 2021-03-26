2b1d421f091ab6f2f98011281fe99073869d37bf
Barrett Weisshaar <ravenium@gmail.com>
2021-03-25 21:29:52 -0700

    added reflecting stream support

--- main.go
+++ main.go
@@ -21,6 +21,9 @@ var srtStreamURL string
 var srtStreamPassword string
 var listenAddr string
 var pollInterval int
+var forwardMode bool
+var reflectURL string
+var processLaunchDelay int
 
 // Stream struct - list of streams in the return from srtrelay
 type Stream []struct {
@@ -45,7 +48,7 @@ func showVideoList(w http.ResponseWriter, req *http.Request) {
 	<video id="video` + strconv.Itoa(i) + `" controls></video>
 	<script>
 	  var video = document.getElementById('video` + strconv.Itoa(i) + `');
-	  var videoSrc = '/` + streams[i].Name + `_srt.m3u8';
+	  var videoSrc = '../` + streams[i].Name + `_srt.m3u8';
 	  if (video.canPlayType('application/vnd.apple.mpegurl')) {
 		video.src = videoSrc;
 	  } else if (Hls.isSupported()) {
@@ -85,11 +88,17 @@ func monitorStreams() {
 	// Initialize maps for each potential ffmpeg process output
 	sprocesses := make(map[string]*exec.Cmd)
 	rprocesses := make(map[string]*exec.Cmd)
+	fprocesses := make(map[string]*exec.Cmd)
 	proctracker := make(map[string]bool)
 
 	ffmpegPath, err := exec.LookPath("ffmpeg")
 	if err != nil {
 		log.Fatal("Cannot find ffmpeg!")
+	}
+
+	sltPath, err := exec.LookPath("srt-live-transmit")
+	if err != nil {
+		log.Fatal("Cannot find srt-live-transmit!")
 	}
 
 	// Poll every interval and launch/kill procs
@@ -139,7 +148,7 @@ func monitorStreams() {
 					sprocesses[streams[i].Name] = exec.Command(ffmpegPath, "-y", "-i", (srtStreamURL + "?streamid=play/" + streams[i].Name + srtStreamPassword), "-c:v", "libx264", "-x264opts", "keyint=1:no-scenecut", "-s", "640x360", "-r", "30", "-b:v", "900k", "-c:a", "aac", "-sws_flags", "bicubic", "-hls_time", "1", "-hls_list_size", "60", "-hls_delete_threshold", "15", "-hls_flags", "delete_segments", ("videos/" + streams[i].Name + "_srt.m3u8"))
 					sprocesses[streams[i].Name].Start()
 					//Wait a second so we don't overload ffmpeg launch
-					time.Sleep(time.Second)
+					time.Sleep(time.Second * time.Duration(processLaunchDelay))
 				}
 				if recordingMode {
 					log.Println("Starting recording job for", streams[i].Name)
@@ -147,15 +156,22 @@ func monitorStreams() {
 					rprocesses[streams[i].Name] = exec.Command(ffmpegPath, "-y", "-i", (srtStreamURL + "?streamid=play/" + streams[i].Name + srtStreamPassword), "-c:v", "copy", "-c:a", "copy", (recordingDir + "/" + streams[i].Name + "_" + t + ".mp4"))
 					rprocesses[streams[i].Name].Start()
 					//Wait a second so we don't overload ffmpeg launch
-					time.Sleep(time.Second)
+					time.Sleep(time.Second * time.Duration(processLaunchDelay))
 
+				}
+				if forwardMode {
+					log.Println("Restreaming ", streams[i].Name, " to ", reflectURL)
+					fprocesses[streams[i].Name] = exec.Command(sltPath, (srtStreamURL + "?streamid=play/" + streams[i].Name + srtStreamPassword), (reflectURL + "?streamid=publish/" + streams[i].Name + srtStreamPassword))
+					fprocesses[streams[i].Name].Start()
+					//Wait a second so we don't overload ffmpeg launch
+					time.Sleep(time.Second * time.Duration(processLaunchDelay))
 				}
 			}
 		}
 
 		for j := range proctracker {
 			if !findStream(streams, j) {
-				log.Println("Removing ffmpeg process for", j)
+				log.Println("Removing ffmpeg processes for", j)
 				delete(proctracker, j)
 				if streamingMode {
 					sprocesses[j].Process.Kill()
@@ -167,6 +183,11 @@ func monitorStreams() {
 					rprocesses[j].Process.Wait()
 					delete(rprocesses, j)
 				}
+				if forwardMode {
+					fprocesses[j].Process.Kill()
+					fprocesses[j].Process.Wait()
+					delete(fprocesses, j)
+				}
 			}
 		}
 	}
@@ -174,13 +195,16 @@ func monitorStreams() {
 
 func main() {
 	flag.StringVar(&srtStatusURL, "serverurl", "http://localhost:8080/streams", "URL of host running SRT status json endpoint")
-	flag.StringVar(&srtStreamURL, "streamurl", "srt://localhost:1935", "IP/port of streaming server")
+	flag.StringVar(&srtStreamURL, "streamurl", "srt://localhost:1935", "URL of streaming server")
 	flag.StringVar(&srtStreamPassword, "playpassword", "", "Password to play srt streams from srtrelay (optional)")
 	flag.StringVar(&listenAddr, "listen", "127.0.0.1:3000", "Listen address for stream viewer")
 	flag.BoolVar(&recordingMode, "r", false, "Record a copy of incoming streams.")
-	flag.BoolVar(&streamingMode, "s", false, "Stream a copy of incoming streams.")
+	flag.BoolVar(&streamingMode, "s", false, "Create a preview copy of incoming streams.")
 	flag.StringVar(&recordingDir, "recpath", "record", "path for recordings (omit trailling slashes)")
 	flag.IntVar(&pollInterval, "poll", 10, "Interval in seconds to poll for new SRT feeds.")
+	flag.BoolVar(&forwardMode, "f", false, "Forward a copy of incoming streams to another SRT server.")
+	flag.StringVar(&reflectURL, "reflect", "srt://localhost:1935", "SRT url to reflect all streams toward.  Usually used in lieu of recording or preview features.")
+	flag.IntVar(&processLaunchDelay, "procdelay", 3, "Delay in seconds between ffmpeg/slt process launches.  Tweak if you are getting process exec failures.")
 	flag.Parse()
 
 	// if we have set a stream passsword, tack it on to the end of the ffmpeg strings above to properly ingest
